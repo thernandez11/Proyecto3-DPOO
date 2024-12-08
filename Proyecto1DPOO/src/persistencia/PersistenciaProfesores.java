8f95fbde03b4e7658d929725addcfbfa1d3d1d5c
thernandez11 <t.hernandez11@uniandes.edu.co>
2024-12-07 19:09:36 -0500

    Arreglo pequeño persistencia profesores
    Porque esto no estaba arreglado para proyecto 2?

--- Proyecto1DPOO/src/persistencia/PersistenciaProfesores.java
+++ Proyecto1DPOO/src/persistencia/PersistenciaProfesores.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.json.JSONArray;
